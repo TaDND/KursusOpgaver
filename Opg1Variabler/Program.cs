218fb715ef5c5275489a793b368a17a4fb98eccc
Dennis <DND@thorslevautomation.dk>
2018-03-08 15:25:32 +0100

    Start

---
+++ Opg1Variabler/Program.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Opg1Variabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            // Your code here.....
+
+
+
+
+            // Keep console window open when using the debugger (F5)
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+}
