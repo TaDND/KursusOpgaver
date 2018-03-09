31f07831ec4f69af49bdcdebc758693122b6bffe
Dennis <DND@thorslevautomation.dk>
2018-03-09 12:32:28 +0100

    Flere farver

--- LoopTabel/Program.cs
+++ LoopTabel/Program.cs
@@ -11,19 +11,42 @@ namespace LoopTabel
             {
                 for (int j = 1; j <= 10; j++)
                 {
-
-                    if (i * j > 50)
+                    if ((i * j)%10==0)
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    else if (i * j > 50)
                         Console.ForegroundColor = ConsoleColor.DarkRed;
+                    else if ((i * j) % 2 == 0)
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
                     else
                         Console.ForegroundColor = ConsoleColor.White;
                     //Console.Write(i*j + "\t");
                     Console.Write((i * j).ToString().PadLeft(5));
-
                 }
                 Console.WriteLine();
             }
             Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
 
+            for (int k = 1; k <= 10; k++)
+            {
+                for (int l = 1; l <= 10; l++)
+                {
+
+                    if ((l * k) % 10 == 0)
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    else if (l * k > 50)
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                    else if ((l * k) % 2 == 0)
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    else
+                        Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(k*l + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
 
 
             // Keep console window open when using the debugger (F5)
