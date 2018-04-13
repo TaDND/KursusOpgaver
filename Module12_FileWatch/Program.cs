9b09ef4abffb4dd9c78b00a3f464235ef2ea2ebf
DND <DND@Thorslevautomation.dk>
2018-04-13 13:03:15 +0200

    Metode tilføjet i Module12_FileWatch

--- Module12_FileWatch/Program.cs
+++ Module12_FileWatch/Program.cs
@@ -14,11 +14,11 @@ namespace Module12_FileWatch
 
             System.IO.FileSystemWatcher w = new System.IO.FileSystemWatcher(@"c:\temp", "*.txt");       //Opretter ny FileWatcher, der kigger på filer i temp-mappe
             w.EnableRaisingEvents = true;       //Enabler events
-            w.Changed += (s, e) => { Console.WriteLine("Filen " + e.Name + " er rettet"); };    //Når er fil ændres bruges lamda til at skrive i konsol.
+            w.Changed += (s, e) => { Console.WriteLine("Filen " + e.Name + " er rettet"); };    //Når er fil ændres bruges lambda til at skrive i konsol.
+            w.Created += W_Created;
 
             do
             {
-
             } while (true);
 
             if (System.Diagnostics.Debugger.IsAttached)
@@ -26,6 +26,11 @@ namespace Module12_FileWatch
                 Console.Write("Press any key to continue . . . ");
                 Console.ReadKey();
             }
+        }
+
+        private static void W_Created(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Console.WriteLine("Filen " + e.Name + " er oprettet");
         }
     }
 }
