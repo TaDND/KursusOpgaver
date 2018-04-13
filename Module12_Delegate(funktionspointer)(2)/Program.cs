b74d866fd894d71408580fc720be4fb7a96d50da
DND <DND@Thorslevautomation.dk>
2018-04-13 10:29:43 +0200

    Module12_Delegate(funktionspointer)(2) løst og kommenteret

--- Module12_Delegate(funktionspointer)(2)/Program.cs
+++ Module12_Delegate(funktionspointer)(2)/Program.cs
@@ -4,18 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Module12_Delegate_Funktionspointer__1_
+namespace Module12_Delegate_funktionspointer__2_
 {
     class Program
     {
-
-        public delegate int Beregn(int a, int b);   //Delegate der returnerer ínt, og som tager 2 int's
-
-
         static void Main(string[] args)
         {
-
-            int res = Beregner(3, 2, Plus);
+            int res=Beregner(3, 2, Plus);
             Console.WriteLine(res);
             res = Beregner(3, 2, Minus);
             Console.WriteLine(res);
@@ -24,6 +19,7 @@ namespace Module12_Delegate_Funktionspointer__1_
             res = Beregner(3, 2, Divider);
             Console.WriteLine(res);
 
+
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 Console.Write("Press any key to continue . . . ");
@@ -31,19 +27,18 @@ namespace Module12_Delegate_Funktionspointer__1_
             }
         }
 
-
-        public static int Beregner (int a, int b, Beregn f)     //Metode der tager 2 int's og en "Beregn" delegate
+        public static int Beregner(int a, int b, Func<int, int, int> f)     //Metode der tager 2 int's og en Func der tager 2 int's og returnerer en int
         {
-            return f.Invoke(a, b);      //Kører delegate med de angivne int's. Er kan være en vilkensomhelst funktion, der blot skal returnere en int, og tager 2 int's som argumenter
+            return f(a, b);      //Kører delegate med de angivne int's. Er kan være en vilkensomhelst funktion, der blot skal returnere en int, og tager 2 int's som argumenter
             //Dette kan kun lade sig gøre, fordi det er en reference til en funktion.
         }
 
-        public static int Plus (int a, int b)
+        public static int Plus(int a, int b)
         {
             return a + b;
         }
 
-        public static int Minus (int a, int b)
+        public static int Minus(int a, int b)
         {
             return a - b;
         }
